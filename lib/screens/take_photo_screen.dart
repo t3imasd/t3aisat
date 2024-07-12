72d007d2db824131f0716cb3c497a2590b7f082c
Pedro Hernández Ruiz <pedroh.r99@gmail.com>
2024-07-12 12:38:38 +0200

    Save photos in the iOS Photo Library and the Document folder inside the App

--- lib/screens/take_photo_screen.dart
+++ lib/screens/take_photo_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:logging/logging.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class TakePhotoScreen extends StatefulWidget {
   const TakePhotoScreen({super.key});
@@ -16,9 +21,34 @@ class TakePhotoScreenState extends State<TakePhotoScreen> {
   // Function to take a photo using the camera
   Future<void> _takePhoto() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.camera);
-    setState(() {
-      _imageFile = image;
-    });
+    if (image != null) {
+      // Save the photo
+      await _savePhoto(image);
+      setState(() {
+        _imageFile = image;
+      });
+    }
+  }
+
+  // Function to save the photo to the device and gallery
+  Future<void> _savePhoto(XFile image) async {
+    final log = Logger('TakePhotoScreen');
+
+    // Save to application directory
+    final Directory appDir = await getApplicationDocumentsDirectory();
+    final String appDirPath = appDir.path;
+    final String fileName = path.basename(image.path);
+    final String savedImagePath = path.join(appDirPath, fileName);
+    await image.saveTo(savedImagePath);
+
+    // Request permission to save to gallery
+    if (await Permission.storage.request().isGranted) {
+      // Save to gallery
+      final result = await ImageGallerySaver.saveFile(savedImagePath);
+      log.info('Image saved to gallery: $result');
+    } else {
+      log.severe('Permission denied to access storage');
+    }
   }
 
   @override
