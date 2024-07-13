efceae1ee1f2cba2d3f9befaaee069291493f4fe
Pedro Hernández Ruiz <pedroh.r99@gmail.com>
2024-07-13 13:04:56 +0200

    Come back to a previous code in the commit titled "Save photos in the iOS Photo Library and the Document folder inside the App"

--- lib/screens/take_photo_screen.dart
+++ lib/screens/take_photo_screen.dart
@@ -6,10 +6,6 @@ import 'package:path/path.dart' as path;
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:logging/logging.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class TakePhotoScreen extends StatefulWidget {
   const TakePhotoScreen({super.key});
@@ -20,10 +16,7 @@ class TakePhotoScreen extends StatefulWidget {
 
 class TakePhotoScreenState extends State<TakePhotoScreen> {
   XFile? _imageFile;
-  Position? _currentPosition;
-  String? _address;
   final ImagePicker _picker = ImagePicker();
-  final Logger log = Logger('TakePhotoScreen');
 
   // Function to take a photo using the camera
   Future<void> _takePhoto() async {
@@ -34,19 +27,13 @@ class TakePhotoScreenState extends State<TakePhotoScreen> {
       setState(() {
         _imageFile = image;
       });
-
-      // Get the current location
-      await _getCurrentLocation();
-      if (_currentPosition != null) {
-        // Get the address from the coordinates
-        await _getAddressFromCoordinates(
-            _currentPosition!.latitude, _currentPosition!.longitude);
-      }
     }
   }
 
   // Function to save the photo to the device and gallery
   Future<void> _savePhoto(XFile image) async {
+    final log = Logger('TakePhotoScreen');
+
     // Save to application directory
     final Directory appDir = await getApplicationDocumentsDirectory();
     final String appDirPath = appDir.path;
@@ -55,72 +42,12 @@ class TakePhotoScreenState extends State<TakePhotoScreen> {
     await image.saveTo(savedImagePath);
 
     // Request permission to save to gallery
-    var status = await Permission.photos.status;
-    if (!status.isGranted) {
-      status = await Permission.photos.request();
-    }
-
-    if (status.isGranted) {
+    if (await Permission.storage.request().isGranted) {
       // Save to gallery
       final result = await ImageGallerySaver.saveFile(savedImagePath);
       log.info('Image saved to gallery: $result');
     } else {
-      log.severe('Permission denied to access photos');
-    }
-  }
-
-  // Function to get the current location
-  Future<void> _getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      log.severe('Location services are disabled.');
-      return;
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        log.severe('Location permissions are denied');
-        return;
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      log.severe('Location permissions are permanently denied');
-      return;
-    }
-
-    _currentPosition = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    log.info('Current position: $_currentPosition');
-
-    // Update the state to reflect the new location
-    setState(() {});
-  }
-
-  // Function to get the address from the coordinates
-  Future<void> _getAddressFromCoordinates(double lat, double lon) async {
-    final apiKey = dotenv.env['MAPBOX_API_KEY'];
-    final url =
-        'https://api.mapbox.com/geocoding/v5/mapbox.places/$lon,$lat.json?access_token=$apiKey';
-
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      if (data['features'].isNotEmpty) {
-        setState(() {
-          _address = data['features'][0]['place_name'];
-        });
-        log.info('Address: $_address');
-      }
-    } else {
-      log.severe('Failed to get address from coordinates');
+      log.severe('Permission denied to access storage');
     }
   }
 
@@ -134,32 +61,12 @@ class TakePhotoScreenState extends State<TakePhotoScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (_imageFile != null)
-              Expanded(
-                child: Image.file(File(_imageFile!.path)),
-              ),
+            if (_imageFile != null) Image.file(File(_imageFile!.path)),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _takePhoto,
               child: const Text('Take Photo'),
             ),
-            if (_currentPosition != null)
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Location: \nLatitude: ${_currentPosition?.latitude}\nLongitude: ${_currentPosition?.longitude}',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            if (_address != null)
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Address: $_address',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            const SizedBox(height: 20),
           ],
         ),
       ),
